a0d4468d0ca38b1264ecca891f22bafcb6bab10b
Leo Zhang <lzsmile505@gmail.com>
2025-08-01 09:44:42 -0400

    Keep only RootInstructions for simplicity

--- core/data/api/frontend/src/App.jsx
+++ core/data/api/frontend/src/App.jsx
@@ -1,10 +1,6 @@
 import "./App.css"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SetsList from "./components/SetsList.js";
-import AliasesList from "./components/AliasesList.js";
-import RDRList from "./components/RDRList.js";
-import RDRDetails from "./components/RDRDetails.js";
-import RootInstructions from "./components/RootInstructions.js";
+import RootInstructions from "./components/RootInstructions.jsx";
 
 const BACKEND_API_URL = "https://data-backend.bed.dev";
 
@@ -19,12 +15,6 @@ export default function App() {
 
       <Routes>
         <Route path="/" element={<RootInstructions backendUrl={BACKEND_API_URL}/>}/>
-        <Route path="/sets" element={<SetsList backendUrl={BACKEND_API_URL}/>}/>
-        <Route path="/aliases" element={<AliasesList backendUrl={BACKEND_API_URL}/>}/>
-        <Route path="/rdrsets/:param" element={<RDRList backendUrl={BACKEND_API_URL} base="rdrsets"/>}/>
-        <Route path="/machines/:param" element={<RDRList backendUrl={BACKEND_API_URL} base="machines"/>}/>
-        <Route path="/rcms/:param" element={<RDRList backendUrl={BACKEND_API_URL} base="rcms"/>}/>
-        <Route path="/rdrs/:param" element={<RDRDetails backendUrl={BACKEND_API_URL}/>}/>
       </Routes>
     </Router>
   );
